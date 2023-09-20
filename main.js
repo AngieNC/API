facf26cc0b30c31bf16b52b63df308de0694a8de
“AngieNC” <angienathalia.32276@gmail.com>
2023-09-20 12:59:12 -0500

    docs: :poop: Codigo sin rumbo
    Se añadio un nuevo archivo js, se actualizo la tabla de HTML y se hicieron modificaciones en index.js

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-let myFrom = document.querySelector("form");
+let myForm = document.querySelector('form');
 let myTabla = document.querySelector("#myData");
 addEventListener("DOMContentLoaded",async()=>{
-    let res = await(await fetch("https://6509e7ebf6553137159c3aee.mockapi.io/API")).json();
+    let res = await(await fetch("https://6509e7ebf6553137159c3aee.mockapi.io/Nomina")).json();
     for (let i = 0; i < res.length; i++){
         myTabla.insertAdjacentHTML("beforeend", `
         <tr>
             <td>${res[i].id}</td>
-            <td>${res[i].cantidad}</td>
+            <td>${res[i].servicios}</td>
+            <td>${res[i].telecomunicaciones}</td>
+            <td>${res[i].comida}</td>
+            <td>${res[i].ocio}</td>
+            <td>${res[i].transporte}</td>
+            <td>${res[i].seguros}</td>
+            <td>${res[i].vivienda}</td>
+            <td>${res[i].opciones}<input type="button" name="boton1" value="actualizar"><input type="button" name="botones" value="eliminar"></td>
         </tr>
     `);
     }
 })
 
-myFrom.addEvenListener('submit', async(e)=> {
+/*
+myForm.addEvenListener('submit', async(e)=> {
     e.preventDefault();
 
     const data = Object.fromEntries(new FormData(e.target));
@@ -23,9 +31,9 @@ myFrom.addEvenListener('submit', async(e)=> {
         Headers:{"content-type": "application/json"},
         body: JSON.stringify(data)
     };
-    let res = await (await fetch("https://6509e7ebf6553137159c3aee.mockapi.io/API", config)).json();
+    let res = await (await fetch("https://6509e7ebf6553137159c3aee.mockapi.io/Nomina", config)).json();
     
     console.log(res);
 
 
-})
+})*/
