0e23ea0fb9d108d4e10f423df7959a4e70a309c2
“AngieNC” <139162495+AngieNC@users.noreply.github.com>
2023-09-20 23:07:03 -0500

    feat: :memo: Creación de archivo .js
    Se creó otro archivo .js para comenzar todo desde cero y tener control de lo que se hace

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-let myForm = document.querySelector('form');
-let myTabla = document.querySelector("#myData");
-addEventListener("DOMContentLoaded",async()=>{
-    let res = await(await fetch("https://6509e7ebf6553137159c3aee.mockapi.io/Nomina")).json();
-    for (let i = 0; i < res.length; i++){
-        myTabla.insertAdjacentHTML("beforeend", `
-        <tr>
-            <td>${res[i].id}</td>
-            <td>${res[i].cantidad}</td>
-            <td>${res[i].botones}</td>
-            <td>${res[i].opciones}<input type="button" name="boton1" value="actualizar"><input type="button" name="botones" value="eliminar"></td>
-        </tr>
-    `);
-    }
-})
 
-/
-myForm.addEvenListener('submit', async(e)=> {
-    e.preventDefault();
-
-    const data = Object.fromEntries(new FormData(e.target));
-    const {cantidad} = data;
-    data.cantidad = (typeof cantidad === "string") ? Number(cantidad): null;
-    let config = {
-        method: "POST",
-        Headers:{"content-type": "application/json"},
-        body: JSON.stringify(data)
-    };
-    let res = await (await fetch("https://6509e7ebf6553137159c3aee.mockapi.io/Nomina", config)).json();
-    
-    console.log(res);
-
-})
+let formulario = document.querySelector('form');
+let tabla = document.querySelector('#myData')
