d0ce7a793ff010d0bbea69ec5a42a26d17b44085
“AngieNC” <139162495+AngieNC@users.noreply.github.com>
2023-09-21 23:56:16 -0500

    docs: :memo: Información nueva
    Se añade nueva información y se arreglan unos métodos en el main.js.

--- main.js
+++ main.js
@@ -1,72 +1,99 @@
 
-let myfrom = document.querySelector("form");
-let myTabla = document.querySelector("#myData");
+// Se declaran las constantes para trabajar con los formularios y la base de datos
+const formulario = document.querySelector("#formAdd");
+const formu = document.querySelector("#formEdit");
+const dialog = document.querySelector("dialog");
+const url = "https://6509e7ebf6553137159c3aee.mockapi.io/Nomina";
 
-//Se llaman los datos
-addEventListener("DOMContentLoaded",async()=>{
-    let res=await(await fetch("https://6509e7ebf6553137159c3aee.mockapi.io/Nomina")).json();
-    for (let i = 0; i< res.length; i++){
-        myTabla.insertAdjacentHTML("beforeend", `
-      <tr>
-      <td>${res[i].id}</td>
-      <td>${res[i].valor}</td>
-      <td>${res[i].caja}</td>
-      </tr>
-      `);
-    
-    }
-})
 
-//Se crean los datos
-async function creacion(data) {
+//Para que funcione el metodo eliminar
+
+const principalEliminar = async(id)=>{
     let config = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
+        method: "DELETE",
+        headers: {"content-type":"application/json"}
     };
-    let res = await (await fetch("https://6509e7ebf6553137159c3aee.mockapi.io/Nomina", config)).json();
-    return res;
+    let res = await (await fetch(url + "/" + id, config)).json();
+
+};
+
+// Para que funcione el metodo editar
+
+const principalEditar = async(id)=>{
+    formu.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        let dato = Object.fromEntries(new FormData(e.target));
+
+        let config = {
+            method: "PUT",
+            headers: {"content-type":"application/json"},
+            body: JSON.stringify(dato)
+        };
+        let res = fetch(url + "/" + id, config);
+
+        dialog.close();
+    })
 }
 
-// Agregar evento para crear un nuevo registro
-myfrom.addEventListener("submit", async (e) => {
+//Todo esto es el evento que se desarrolla después de presionar 'ENVIAR'
+
+formulario.addEventListener("submit", async(e)=>{
     e.preventDefault();
-    const data = Object.fromEntries(new FormData(e.target));
-    const { valor } = data;
-    data.valor = (typeof valor === "string") ? Number(valor) : null;
-  
-    try {
-      let createdData = await creacion(data);
-      console.log("Registro creado:", createdData);
-    } catch (error) {
-      console.error("Error al crear el registro:", error);
+    
+    let dato = Object.fromEntries(new FormData(e.target));
+    
+    const peticion = await (await fetch(url)).json();
+
+    let config = {
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(dato)
     }
+
+    const enviar = await (await fetch(url, config)).json();
+    
+    location.reload();
 });
 
-//Se cargan los datos
-async function cargar() {
-    try {
-      let data = await fetchData();
-      for (let i = 0; i < data.length; i++) {
-        myTabla.insertAdjacentHTML("beforeend", `
-          <tr>
-            <td>${data[i].id}</td>
-            <td>${data[i].valor}</td>
-            <td>${data[i].caja}</td>
+// Descarga todo el contenido como el HTML o los estilos 
+
+document.addEventListener("DOMContentLoaded", async(e)=>{
+    const tabla = document.querySelector('#data-tabla');
+    let res = await (await fetch(url)).json();
+    
+    //Crea un nuevo array y manipula el array que recorre.
+    res.map((elemento)=>{
+        tabla.insertAdjacentHTML("beforeend", `
+        <tr>
+            <td>${elemento.id}</td>
+            <td>${elemento.valor}</td>
+            <td>${elemento.caja}</td>
             <td>
-              <button class="btn-edit" data-id="${data[i].id}">Editar</button>
-              <button class="btn-delete" data-id="${data[i].id}">Eliminar</button>
+                <button id="${elemento.id}" class="edit">Editar</button>
+                <button id="${elemento.id}" class="delet">Eliminar</button>
             </td>
-          </tr>
-        `)};
-      
-    }catch (error) {
-        console.error("Error al crear el registro:", error);
-    }
-}
+        </tr>
+        `)
+    });
 
+    const eliminar = document.querySelector('.delet');
+    
+    const editar = document.querySelector('.edit');
+    
+    //Evento eliminar
+    eliminar.forEach((elemento) =>{
+        elemento.addEventListener("click",()=>{
+            // console.log(typeof element.id)
+            principalEliminar(elemento.id);
+        })
+    });
 
-// Llamar a la función para cargar datos al iniciar la página y ellos se agregan al final
-cargar();
+    //Evento editar
+
+    editar.forEach((elemento)=>{
+        elemento.addEventListener("click",(event)=>{
+            dialog.showModal();
+            principalEditar(elemento.id);
+        })
+    });
+});
